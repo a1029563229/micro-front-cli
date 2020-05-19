d95d7a88492bd6c431e02030c49ce984c63793d2
lt <1029563229@qq.com>
2020-05-19 10:47:58 +0800

    ✨ 添加了几个插件

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const { program } = require("commander");
 const package = require("../package.json");
 const create = require("./create");
 
-program.version(package.version).usage("<command> [options]");
-
 program
+  .version(package.version, "-v, --version")
   .command("create <app-name>")
   .description("create a new project powered by micro-front-cli")
   .action((name) => {
